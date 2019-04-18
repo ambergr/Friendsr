967e922b11c9864dd81256d03c109efbb7130bad
ambergr <a.mayenburg@hotmail.com>
2019-04-18 15:10:10 +0200

    Final version + comments

--- app/src/main/java/com/example/friendsr/MainActivity.java
+++ app/src/main/java/com/example/friendsr/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+        //connect this listener to the GridView.
         GridView gv = findViewById(R.id.grid);
         gv.setOnItemClickListener(new GridItemClickListener());
 
